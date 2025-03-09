f1867ce760dcb35d687b071f6e3fc43632c7083c
Mohammed Alrufaihi <malrufaihi@gmail.com>
2025-03-09 08:10:45 +0300

    enh: simplified eslint

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -6,28 +6,15 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended"
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
     sourceType: "module",
   },
-  ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
-    "/generated/**/*", // Ignore generated files.
-  ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
   rules: {
-    "quotes": ["error", "double"],
-    "import/no-unresolved": 0,
-    "indent": ["error", 2],
-  },
+    "no-debugger": "warn",  // Change from error to warning
+    "@typescript-eslint/no-explicit-any": "warn", // Allow 'any' with warning
+    "@typescript-eslint/no-var-requires": "off"  // Shut up about CommonJS requires
+  }
 };
